9f7751fc0158454dab635f0e020aef91c8abef96
Gaurav Beniwal <gaurav.beniwal9233@gmail.com>
2024-11-20 19:14:00 +0000

    user registration

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,23 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+@RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerAccount (@RequestBody Account account) {
+        try {
+            Account registeredAccount = accountService.registerAccount(account.getUsername(), account.getPassword());
+            return ResponseEntity.ok(registeredAccount); //200
+        } catch (IllegalArgumentException e) {
+            if (e.getMessage().equals("Username already exists")) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage()); //409
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage()); //400
+        }
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,29 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account registerAccount(String username, String password) throws IllegalArgumentException {
+        if (username == null || username.trim().isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be balnk");
+        }
+        if (password == null || password.length() < 4) {
+            throw new IllegalArgumentException("Password must be at least 4 characters long");
+        }
+        if (accountRepository.existsByUsername(username)) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        Account newAccount = new Account(username, password);
+        return accountRepository.save(newAccount);
+    }
+
 }
